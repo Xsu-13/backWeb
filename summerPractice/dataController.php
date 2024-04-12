9f60c542550a95b4e3346d27edc86302f24ab5ca
Kseniia Kosonogova <k.kosonogova@xlab-game.com>
2024-04-12 20:22:30 +0300

    update film 1.2

--- summerPractice/dataController.php
+++ summerPractice/dataController.php
@@ -17,6 +17,7 @@
           } 
           
         if(isset($_POST["Edit"])){
+            $currentFilm = array();
             $currentFilm = GetFilmById($db, $id);
             include('editFilm.php');
           } 
