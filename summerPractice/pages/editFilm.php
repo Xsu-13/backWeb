6d91d657c13cfcce812e9ea1de729010dcb713b3
Kseniia Kosonogova <k.kosonogova@xlab-game.com>
2024-04-16 01:47:00 +0300

    fix film editing

--- summerPractice/pages/editFilm.php
+++ summerPractice/pages/editFilm.php
@@ -12,19 +12,19 @@
         <form action="" method="POST">
             <input name="film_id" value="<?php echo $currentFilm['film_id']; ?>" type="hidden" />
             <label for="title">Название:</label>
-            <input type="text" id="title" name="title" value="<?php echo $currentFilm['title']; ?>" required>
+            <input type="text" id="title" name="film_title" value="<?php echo $currentFilm['title']; ?>" required>
     
             <label for="director">Режиссёр:</label>
-            <input type="text" id="director" name="director" value="<?php echo $currentFilm['director']; ?>" required>
+            <input type="text" id="director" name="film_director" value="<?php echo $currentFilm['director']; ?>" required>
     
             <label for="year">Год выпуска:</label>
-            <input type="number" id="year" name="year" value="<?php echo $currentFilm['year']; ?>" required>
+            <input type="number" id="year" name="film_year" value="<?php echo $currentFilm['year']; ?>" required>
     
             <label for="genre">Жанр:</label>
-            <input type="text" id="genre" name="genre" value="<?php echo $currentFilm['genre']; ?>" required>
+            <input type="text" id="genre" name="film_genre" value="<?php echo $currentFilm['genre']; ?>" required>
     
             <label for="description">Описание:</label>
-            <textarea id="description" name="description" required><?php echo $currentFilm["description"]?></textarea>
+            <textarea id="description" name="film_description" required><?php echo $currentFilm["description"]?></textarea>
     
             <input type="submit" value="Изменить" name="UpdateFilm">
         </form>
