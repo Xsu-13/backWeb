f9f4e71dca7e0df3bcb4088f55fe52d7951a7b20
xsu <xsusha03@gmail.com>
2024-05-04 13:42:40 +0300

    a

--- DanialSummer/database/databaseService.php
+++ DanialSummer/database/databaseService.php
@@ -90,6 +90,7 @@ function SaveProduct($db)
         try{
             $sth = $db->prepare('SELECT DishID, d.Title as dishTitle, Description, Price, m.Title as menuTitle, d.MenuID as MenuID FROM Dishes d Join Menu m on d.MenuID = m.MenuID');
             $dishes = array();
+            $k = 0;
             $result = $sth->execute();
             $row = $sth->fetchAll();
             for($h = 0; $h < count($row); $h++) {
